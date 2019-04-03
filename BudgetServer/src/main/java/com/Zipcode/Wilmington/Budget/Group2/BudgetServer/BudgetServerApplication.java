b6ca7796beb3aa779f307ac8dff378f0cd21dbce
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-04-03 09:32:24 -0400

    added spingboot config

---
+++ BudgetServer/src/main/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/BudgetServerApplication.java
@@ -0,0 +1,13 @@
+package com.Zipcode.Wilmington.Budget.Group2.BudgetServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetServerApplication.class, args);
+	}
+
+}
---
+++ BudgetServer/src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/BudgetServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Zipcode.Wilmington.Budget.Group2.BudgetServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudgetServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
