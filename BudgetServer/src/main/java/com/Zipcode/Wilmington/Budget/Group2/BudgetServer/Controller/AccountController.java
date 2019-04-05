fa108c698ee16068ba9ceda225c4a7f629270c46
Michael Krohn <michaelkrohn@zipcoders-MacBook-Pro-96.local>
2019-04-05 09:30:49 -0400

    Added empty spring components

---
+++ BudgetServer/src/main/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Controller/AccountController.java
@@ -0,0 +1,7 @@
+package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+}
---
+++ BudgetServer/src/main/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Entity/User.java
@@ -0,0 +1,7 @@
+package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+}
