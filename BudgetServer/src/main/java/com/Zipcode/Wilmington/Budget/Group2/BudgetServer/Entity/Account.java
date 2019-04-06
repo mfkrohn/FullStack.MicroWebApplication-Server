62646af4ef36c1caac073301cc3c732b5dbdfe92
Michael Krohn <michaelkrohn@zipcodersMBP96.fios-router.home>
2019-04-06 08:49:34 -0400

    Added lombok dependency and modified account entity class to include lombok annotations

--- BudgetServer/src/main/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Entity/Account.java
+++ BudgetServer/src/main/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Entity/Account.java
@@ -1,38 +1,30 @@
 package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @NotNull
     private Integer userID;
 
+    @NotNull
     private Double balance;
 
-    public Integer getId() {
-        return id;
+    public Account() {
     }
 
-    public Integer getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Integer userID) {
+    public Account(@NotNull Integer userID, @NotNull Double balance) {
         this.userID = userID;
-    }
-
-    public Double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Double balance) {
         this.balance = balance;
     }
+
 }
