94657b1ad82f59b6012600acc42bd64530e8a5ee
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-04-16 19:48:55 -0400

    created testDeleteUser method

--- src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/service/UserServiceTest.java
+++ src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/service/UserServiceTest.java
@@ -3,11 +3,10 @@ package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.service;
 import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Entity.User;
 import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Repositories.UserRepo;
 import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Service.UserService;
+
 import org.junit.Assert;
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class UserServiceTest {
 
@@ -16,19 +15,53 @@ public class UserServiceTest {
         // given
         Integer expectedId = 1;
         String expectedName = "Julian";
-        User user = new User(expectedId, "kjskdjaskdjskl");
 
         UserRepo mockRepo = mock(UserRepo.class);
-
         User userToReturn = new User(expectedId, expectedName);
-//        userToReturn.setName("Julian");
 
         // when
-        when(mockRepo.save(user)).thenReturn(userToReturn);
+        when(mockRepo.save(userToReturn)).thenReturn(userToReturn);
         UserService service = new UserService(mockRepo);
-        User actual = service.create(user);
+        User actual = service.create(userToReturn);
 
         // then
         Assert.assertEquals(expectedName, actual.getName());
     }
+
+    @Test
+    public void testUpdateName() {
+        // given
+        Integer expectedId = 1;
+        String expectedName = "Sam";
+        User userToUpdate = new User(1, "jkldsjaklsdj");
+
+        UserRepo mockRepo = mock(UserRepo.class);
+
+        // when
+        UserService service = new UserService(mockRepo);
+        service.create(userToUpdate);
+        service.updateName(userToUpdate, expectedId);
+
+        // then
+        verify(mockRepo).save(userToUpdate);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        // given
+        Integer expectedId = 1;
+        User userToDelete = new User(expectedId, "kdjaksjd");
+
+        UserRepo mockRepo = mock(UserRepo.class);
+        // when
+        UserService service = new UserService(mockRepo);
+        service.create(userToDelete);
+
+        // then
+        Assert.assertTrue(service.deleteUser(expectedId));
+    }
 }
+
+
+// @MvcTest
+// @RunWith(SpringRunner.class)
