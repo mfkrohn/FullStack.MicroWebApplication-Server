41dcb2c04d21647ef084f6288fe79f29a7929d0d
Julian Rios <julianrios@zipcoders-MacBook-Pro-10.local>
2019-04-17 16:54:46 -0400

    created test for create, delete, get, and update account

--- src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Service/AccountServiceTest.java
+++ src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Service/AccountServiceTest.java
@@ -1,12 +1,15 @@
 package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Service;
 
+import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Entity.Account;
 import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Repositories.AccountRepo;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Optional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -25,7 +28,44 @@ public class AccountServiceTest {
     }
 
     @Test
-    public void testCreate() {
+    public void testCreateAccount() {
+        Account account = new Account(1, 100.00);
+        Mockito.when(repo.save(account)).thenReturn(account);
 
+        service.create(account);
+
+        Mockito.verify(repo, Mockito.times(1)).save(account);
     }
+
+    @Test
+    public void testDeleteAccount() {
+        Account account = new Account(1, 10.00);
+        Mockito.when(repo.findById(1)).thenReturn(Optional.of(account));
+
+        service.delete(1);
+
+        Mockito.verify(repo, Mockito.times(1)).deleteById(1);
+    }
+
+    @Test
+    public void testGetAccount() {
+        Account account = new Account(1, 1000.00);
+        Mockito.when(repo.findById(1)).thenReturn(Optional.of(account));
+
+        service.getAccount(1);
+
+        Mockito.verify(repo, Mockito.times(1)).findById(1);
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        Account account = new Account(1, 10.00);
+        Account account2 = new Account(1, 100.00);
+        Mockito.when(repo.findById(1)).thenReturn(Optional.of(account));
+
+        service.update(1, account2);
+
+        Mockito.verify(repo, Mockito.times(1)).findById(1);
+    }
+
 }
