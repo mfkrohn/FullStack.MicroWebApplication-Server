e5db80eb707bf5f1a2909d5806610738f54ab397
Julian Rios <julianrios@zipcoders-MacBook-Pro-10.local>
2019-04-17 21:58:01 -0400

    created tests for equals and hashcode in profile entity

---
+++ src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Exceptions/NoSuchEntityExceptionTest.java
@@ -0,0 +1,11 @@
+package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Exceptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoSuchEntityExceptionTest {
+    @Test
+
+
+}
