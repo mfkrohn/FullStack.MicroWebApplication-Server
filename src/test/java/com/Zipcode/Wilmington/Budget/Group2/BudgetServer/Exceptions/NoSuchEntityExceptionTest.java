66f76b6d1a78f3879d5352b2c3571dcc5126e55b
Julian Rios <julianrios@zipcoders-MacBook-Pro-10.local>
2019-04-17 22:29:25 -0400

    created TestNoSuchEntityException method

--- src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Exceptions/NoSuchEntityExceptionTest.java
+++ src/test/java/com/Zipcode/Wilmington/Budget/Group2/BudgetServer/Exceptions/NoSuchEntityExceptionTest.java
@@ -1,11 +1,17 @@
 package com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Exceptions;
 
+import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Entity.Account;
+import com.Zipcode.Wilmington.Budget.Group2.BudgetServer.Entity.Profile;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class NoSuchEntityExceptionTest {
-    @Test
 
+    @Test(expected = NoSuchEntityException.class)
+    public void TestNoSuchEntityException() throws NoSuchEntityException {
+        Object object = new Object();
 
+        if(!(object instanceof Account || object instanceof Profile)) {
+            throw new NoSuchEntityException("thrown");
+        }
+    }
 }
